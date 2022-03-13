e09a6cd9d485d97fc2a59796115ac149606470ff
Kamil Samigullin <kamil@samigullin.info>
2022-03-13 08:53:22 +0300

    issue #4: ci/cd: add golangci-lint healthcheck script

---
+++ .github/actions/check/golangci-lint.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+@error() { echo "${@}" >&2; }
+@fatal() { @error "${@}" && exit 1; }
+
+curl -sSfL https://install.octolab.org/golangci-lint |
+  sh -s -- -b /usr/local/bin -dx
+
+expected="$(
+  gh repo view kamilsk/golangci-lint \
+    --json latestRelease --jq .latestRelease.tagName
+)"
+obtained="v$(golangci-lint --version | awk -F' ' '{print $4}')"
+
+[ "${obtained}" == "${expected}" ] || @fatal "expected ${expected}, obtained ${obtained}"
