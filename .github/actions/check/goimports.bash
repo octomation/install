aca418cac15a973e5f3d294504f501ebb17c04c7
Kamil Samigullin <kamil@samigullin.info>
2022-03-13 08:46:35 +0300

    issue #4: ci/cd: add goimports healthcheck script

---
+++ .github/actions/check/goimports.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+@error() { echo "${@}" >&2; }
+@fatal() { @error "${@}" && exit 1; }
+
+curl -sSfL https://install.octolab.org/goimports |
+  sh -s -- -b /usr/local/bin -dx
+
+expected="$(
+  gh repo view kamilsk/go-tools \
+    --json latestRelease --jq .latestRelease.tagName
+)"
+obtained="v$({ goimports -version test 2>/dev/null || true; } | awk -F, '{print $1}')"
+
+[ "${obtained}" == "${expected}" ] || @fatal "expected ${expected}, obtained ${obtained}"
