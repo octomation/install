e4e1eacc4a45c049df00981ec69a36b8274b1f84
Kamil Samigullin <kamil@samigullin.info>
2022-03-27 08:56:54 +0300

    feat: support remote installation by @install and update readme

--- bin/lib/tools/_.bash
+++ bin/lib/tools/_.bash
@@ -7,9 +7,20 @@ if [[ ":${PATH}:" != *":${_BINPATH}:"* ]]; then
 fi
 [ -d "${_BINPATH}" ] || mkdir -p "${_BINPATH}"
 
+# Example: run @install dist/vendor/gum
+# Example: run @install dist/vendor/gum ./bin/{os}/{arch}
+# Example: run @install https://install.octolab.org/vendor/gum
+# Example: run @install https://install.octolab.org/vendor/gum ./bin/{os}/{arch}
 @install() {
   local script=${1} path=${2:-${_BINPATH}}
   path=${path/'{os}'/$(@os)}
   path=${path/'{arch}'/$(@arch)}
+  if [[ "${script}" =~ ^https://install.octolab.org/vendor ]]; then
+    curl -sSfL "${script}" | bash -s -- "${path}"
+    return $?
+  elif [[ "${script}" =~ ^https://install.octolab.org ]]; then
+    curl -sSfL "${script}" | sh -s -- -b "${path}"
+    return $?
+  fi
   ${script} "${path}"
 }
